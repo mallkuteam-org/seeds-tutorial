155b4ee51d89d15b53a07aa68703a139dc27bbda
Max Lezano <david.lezano@gmail.com>
2021-11-08 19:11:58 -0300

    agrega al diccionario fun

--- diccionario_autos.py
+++ diccionario_autos.py
@@ -11,7 +11,13 @@ def main():
      'asientos': 2,
      'puertas': 2}
 
-    autos = [focus, corsa]
+    fun = {'marca': 'Susuki',
+     'modelo': 'Fun',
+     'year': 2015,
+     'asientos': 4,
+     'puertas': 3}
+
+    autos = [focus, corsa, fun]
 
     for auto in autos:
         print(auto['marca'])
